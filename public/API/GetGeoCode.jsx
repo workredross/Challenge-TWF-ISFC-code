18e4e4d5eee54c9af463433bb277db75991ddde7
Kanishk Srivastava <workredross@gmail.com>
2018-02-11 16:19:10 +0530

    For GitHub

--- public/API/GetGeoCode.jsx
+++ public/API/GetGeoCode.jsx
@@ -1,10 +1,10 @@
 var axios = require('axios');
-
-const URL = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyALtmPBjLMXNCFxH99_aTLsZhmGUz9oDqM&address=';
+const keys = require('./key.js');
+const URL = 'https://maps.googleapis.com/maps/api/geocode/json?key=';
 
 module.exports = {
   getGeoCode: function (location) {
-    var requestUrl = `${URL}${location}`;
+    var requestUrl = `${URL}${keys.googleAPIkey}&address=${location}`;
     return axios.get(requestUrl).then(function (res) {
       if (res.data.cod && res.data.message) {
         throw new Error(res.data.message);
